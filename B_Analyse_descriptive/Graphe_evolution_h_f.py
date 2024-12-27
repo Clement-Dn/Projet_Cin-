48d4878d2281ae71823c3a827ea6b07d0aeb06c0
lelkamel <lyna.elkamel@ensae.fr>
2024-12-27 11:57:12 +0000

    changement du graphique évolution

--- B_Analyse_descriptive/Graphe_evolution_h_f.py
+++ B_Analyse_descriptive/Graphe_evolution_h_f.py
@@ -9,9 +9,7 @@ import pandas as pd
 
 #############################################################  Courbes pour l'évolution dans le tps nb de films h vs f
 
-
-
-def evolution_f_h(dataframe, genre):
+def evolution_f_h_cnc(dataframe, genre):
     """ 
     
     Affichage de l'évolution dans le temps du nombre de films en fonction du genre du rélisateurs pour un GENRE de film demandé (si non renseigné tous les genres sont pris en compte)
@@ -20,10 +18,10 @@ def evolution_f_h(dataframe, genre):
 
     # Création de toutes les combinaisons (année,genre_ind) afin de pouvoir injecter par la suite les combinaisons manquantes     
     # Index de toutes les années et modalités
-    all_years = dataframe['annee'].unique()
+    all_years = dataframe['date'].unique()
     all_genres = dataframe['genre_ind'].unique()
 
-    combinaisons = pd.MultiIndex.from_product([all_years, all_genres], names=['annee', 'genre_ind'])
+    combinaisons = pd.MultiIndex.from_product([all_years, all_genres], names=['date', 'genre_ind'])
     combinaisons = pd.DataFrame(index=combinaisons).reset_index()
     combinaisons['initialisation'] = 0
 
@@ -31,17 +29,17 @@ def evolution_f_h(dataframe, genre):
 
     # Sélection du genre de film a prendre en compte
     if genre != 'Tous':
-        dataframe = dataframe.loc[dataframe['genre1'] == genre]
+        dataframe = dataframe.loc[dataframe['genre'] == genre]
         titre = f'Nb de films par genre du/des réalisateur/s pour les films {genre}'
     else:
         titre = 'Nb de films par genre du/des réalisateur/s' 
 
 
-    groupby = dataframe.groupby(['annee', 'genre_ind']).size().reset_index(name='count')
-    groupby_merged = combinaisons.merge(groupby, on=['annee', 'genre_ind'], how='left').fillna(0)
+    groupby = dataframe.groupby(['date', 'genre_ind']).size().reset_index(name='count')
+    groupby_merged = combinaisons.merge(groupby, on=['date', 'genre_ind'], how='left').fillna(0)
 
     # Pivoter afin d'avoir les années en index et les modalités en colonnes
-    transposee = groupby_merged.pivot(index='annee', columns='genre_ind', values='count').fillna(0)
+    transposee = groupby_merged.pivot(index='date', columns='genre_ind', values='count').fillna(0)
 
   
     # Courbes
@@ -57,7 +55,8 @@ def evolution_f_h(dataframe, genre):
 
 
 
-def graphique_h_f(dataframe):
+
+def graphique_h_f_cnc(dataframe):
     """ 
     
     Graphique interactif du nombre de films par réalisateur f, m, ou bien avec plusieurs réalisateurs en fonction du type de film
@@ -66,12 +65,12 @@ def graphique_h_f(dataframe):
     # Annees considérées
     annee_conservees = list(range(2010, 2023))
     annees_string = [str(annee) for annee in annee_conservees]
-    dataframe = dataframe[dataframe['annee'].isin(annees_string)]
+    dataframe = dataframe[dataframe['date'].isin(annees_string)]
 
 
     # On ne conserve que les genres de films qui sont assez nombreux (fixé à > 20)
-    modalites_presentes = dataframe['genre1'].value_counts()
-    modalites_suffisantes = list(modalites_presentes[modalites_presentes >= 20].index)
+    modalites_presentes = dataframe['genre'].value_counts()
+    modalites_suffisantes = list(modalites_presentes.index)
     modalites_suffisantes.append('Tous')
 
 
@@ -89,11 +88,12 @@ def graphique_h_f(dataframe):
             clear_output(wait=True) # supprimer l'ancien graphe si un nouveau est demandé
 
             display(menu_deroulant)
-            evolution_f_h(dataframe, change['new'])
+            evolution_f_h_cnc(dataframe, change['new'])
 
 
     # Associer le bouton à la fonction de mise à jour
     menu_deroulant.observe(update_genre_film)
 
     display(menu_deroulant)
-    evolution_f_h(dataframe, menu_deroulant.value)
+    evolution_f_h_cnc(dataframe, menu_deroulant.value)
+
